f051a4a684625a1cd60e7b81d4efa6625c372e61
rlJoshua <31963935+rlJoshua@users.noreply.github.com>
2019-10-16 13:52:55 +0200

    Ajout de index.php dans un dossier
    Ajout du fichier index.php dans le dossier add, envoie deu mail toujours impossible

---
+++ app/index.php
@@ -0,0 +1,3 @@
+<?php
+mail("recipient", "subject", "message", "From: Sender");
+//mail("test@joshua.com", "test", time(), "From: Mailhog <mailhog@example.com>");
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-//mail("recipient", "subject", "message", "From: Sender");
-mail("test@joshua.com", "test", time(), "From: Mailhog <mailhog@example.com>");
